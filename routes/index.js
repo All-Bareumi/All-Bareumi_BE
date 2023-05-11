564118b0445d8e101ed001fc534a0a56eac9e57c
bigstar9906 <bigstar9906@naver.com>
2023-05-11 13:13:33 +0900

    Fix : Test Router 추가

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const learningRouter = require('./learning')
+const testRouter = require('./test')
 const router = express.Router();
 
 router.use('/learning',learningRouter)
+router.use('/test',testRouter)
 
 module.exports = router
